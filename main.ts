1dded4d042ebb7360e65f64495951d69a23beffb
alainlavanchy <alain.lavanchy@gmail.com>
2020-11-20 09:54:33 +0100

    Calibration of 2 sensors

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ function set_val_null () {
     sensor_12_high = 0
     sensor_12_low = 0
     sensor_12 = 0
+    button_a = 0
 }
 function make_null () {
     pins.digitalWritePin(DigitalPin.P8, 0)
@@ -10,7 +11,10 @@ function make_null () {
     pins.digitalWritePin(DigitalPin.P5, 0)
     pins.digitalWritePin(DigitalPin.P6, 0)
 }
-function calibrate_pin (Pin: number) {
+input.onButtonPressed(Button.A, function () {
+    button_a = 1
+})
+function calibrate_pins (Pin: number) {
     pins.digitalWritePin(DigitalPin.P8, 1)
     basic.pause(100)
     sensor_12_high = pins.analogReadPin(AnalogPin.P0)
@@ -22,22 +26,44 @@ function calibrate_pin (Pin: number) {
         . . . . .
         . . . . .
         `)
-    basic.pause(2000)
+    control.waitForEvent(button_a, 1)
+    basic.pause(100)
     sensor_12_low = pins.analogReadPin(AnalogPin.P0)
     pins.digitalWritePin(DigitalPin.P8, 0)
     basic.showIcon(IconNames.Yes)
+    button_a = 0
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P7, 1)
+    basic.pause(100)
+    sensor_3_high = pins.analogReadPin(AnalogPin.P0)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    control.waitForEvent(button_a, 1)
+    basic.pause(100)
+    sensor_3_low = pins.analogReadPin(AnalogPin.P0)
+    basic.showIcon(IconNames.Yes)
+    button_a = 0
 }
 let sensor_sum = 0
 let sensor_9 = 0
 let sensor_6 = 0
 let sensor_3 = 0
+let sensor_3_low = 0
+let sensor_3_high = 0
+let button_a = 0
 let sensor_12 = 0
 let sensor_12_low = 0
 let sensor_12_high = 0
 let threshold = 0
 make_null()
 set_val_null()
-calibrate_pin(8)
+calibrate_pins(8)
 basic.forever(function () {
     make_null()
     basic.showLeds(`
@@ -49,11 +75,11 @@ basic.forever(function () {
         `)
     pins.digitalWritePin(DigitalPin.P8, 1)
     basic.pause(100)
-    sensor_12 = pins.analogReadPin(AnalogPin.P0)
+    sensor_12 = Math.map(pins.analogReadPin(AnalogPin.P0), sensor_12_low, sensor_12_high, 0, 10)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P7, 1)
     basic.pause(100)
-    sensor_3 = pins.analogReadPin(AnalogPin.P0)
+    sensor_3 = Math.map(pins.analogReadPin(AnalogPin.P0), sensor_3_low, sensor_3_high, 0, 10)
     pins.digitalWritePin(DigitalPin.P7, 0)
     pins.digitalWritePin(DigitalPin.P5, 1)
     basic.pause(100)
